fbfa576ba7567c417d20d2bde4d353ab6d85ce70
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2021-10-22 11:57:38 +0200

    Update hyper

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -20,6 +20,9 @@ module.exports = {
             startX: 25,
             startY: 50
         },
+        hyperTransparentDynamic: {
+            alpha: 0.8
+        },
         // choose either `'stable'` for receiving highly polished,
         // or `'canary'` for less polished but more frequent updates
         updateChannel: 'stable',
@@ -117,7 +120,7 @@ module.exports = {
         // Supported Options:
         //  1. 'SOUND' -> Enables the bell as a sound
         //  2. false: turns off the bell
-        bell: 'SOUND',
+        bell: false,
         // An absolute file path to a sound file on the machine.
         // bellSoundURL: '/path/to/sound/file',
         // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
