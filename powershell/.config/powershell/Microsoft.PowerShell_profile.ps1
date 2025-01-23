95788455e13041a963a55b6325f8c2dc70134bb0
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2025-01-23 08:03:07 +0100

    Update bin, powershell

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,30 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\spaceship.omp.json" | Invoke-Expression
 
+Set-Alias -Name l -Value Clear-Host
+
 function Get-LongListing {
     Get-ChildItem -Path . -Force | Sort-Object LastWriteTime -Descending | Format-Table -AutoSize
 }
+Set-Alias -Name ll -Value Get-LongListing
+
+#######
+
+#function Connect-VM {
+#    Enter-PSSession -HostName devserver.leonardo.local -UserName "LEONARDO\Administrator" -SSHTransport -Port 2222
+#}
 
 function Connect-VM {
-    Enter-PSSession -HostName devserver.leonardo.local -UserName "LEONARDO\Administrator" -SSHTransport -Port 2222
+    $CustomProfilePath = "C:\Users\administrator.LEONARDO\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+    $session = New-PSSession -HostName devserver.leonardo.local -UserName "LEONARDO\Administrator" -SSHTransport -Port 2222
+    Invoke-Command -Session $session -ScriptBlock {
+        param($ProfilePath)
+        if (Test-Path $ProfilePath) {
+            . $ProfilePath
+        } else {
+            Write-Host "Profile path not found: $ProfilePath"
+        }
+    } -ArgumentList $CustomProfilePath
+    Enter-PSSession -Session $session
 }
 
-Set-Alias -Name l -Value Clear-Host
-Set-Alias -Name ll -Value Get-LongListing
 Set-Alias -Name vm -Value Connect-VM
