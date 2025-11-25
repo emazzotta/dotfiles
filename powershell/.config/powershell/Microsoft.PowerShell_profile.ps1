4b6f3954b0e583ee4312bce4122987f219beff8a
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2025-11-25 20:35:21 +0100

    Update bash, powershell, ssh

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\spaceship.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/spaceship.omp.json" | Invoke-Expression
 
 Set-Alias -Name l -Value Clear-Host
 
@@ -13,11 +13,12 @@ function ..3 { cd ..; cd ..; cd .. }
 function ..4 { cd ..; cd ..; cd ..; cd .. }
 
 function Connect-Devserver-VM {
-    # Enter-PSSession -HostName devserver.leonardo.local -UserName "LEONARDO\Administrator" -SSHTransport -Port 2222
+    # $ Enter-PSSession -HostName devserver.leonardo.local -UserName "LEONARDO\Administrator" -SSHTransport -Port 2222
     $CustomProfilePath = "C:\Users\administrator.LEONARDO\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
     $session = New-PSSession -HostName devserver.leonardo.local -UserName "administrator" -SSHTransport -Port 2222
     Invoke-Command -Session $session -ScriptBlock {
         param($ProfilePath)
+        Set-Location ~
         if (Test-Path $ProfilePath) {
             . $ProfilePath
         } else {
@@ -27,13 +28,15 @@ function Connect-Devserver-VM {
     Enter-PSSession -Session $session
 }
 
-# TODO: Setup passwordless access
 function Connect-Devhost-VM {
-    # Enter-PSSession -HostName 192.168.192.150 -UserName "dev-server\administrator" -SSHTransport -Port 2222
+    # $ Enter-PSSession -HostName 192.168.192.150 -UserName "dev-server\administrator" -SSHTransport -Port 2222
+    # # Copy your public key to the other server
+    # $ Add-Content -Path C:\ProgramData\ssh\administrators_authorized_keys -Value "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQ... your_key_here"
     $CustomProfilePath = "C:\Users\Administrator\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
     $session = New-PSSession -HostName 192.168.192.150 -UserName "administrator" -SSHTransport -Port 2222
     Invoke-Command -Session $session -ScriptBlock {
         param($ProfilePath)
+        Set-Location ~
         if (Test-Path $ProfilePath) {
             . $ProfilePath
         } else {
@@ -43,5 +46,5 @@ function Connect-Devhost-VM {
     Enter-PSSession -Session $session
 }
 
-Set-Alias -Name dsrv -Value Connect-Devserver-VM
-Set-Alias -Name dhost -Value Connect-Devhost-VM
+Set-Alias -Name wdev -Value Connect-Devserver-VM
+Set-Alias -Name whost -Value Connect-Devhost-VM
